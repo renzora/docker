81229a2e2fc9027aceab40972f6e991d704b54f8
renzora <106055871+renzora@users.noreply.github.com>
2024-03-08 23:59:58 +0000

    added new install link

--- setup.sh
+++ setup.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-
-# Clone the web repository and install Composer dependencies
+git clone https://github.com/renzora/dev
+cd dev
 git clone https://github.com/renzora/web
-cd web
-composer install
-cd ..
-
-# Clone the server repository and install Node.js dependencies
 git clone https://github.com/renzora/ws
-cd ws
-npm install
-cd ..
-
-# Clone the server repository and install Node.js dependencies
 git clone https://github.com/renzora/server
-cd server
-npm install
-cd ..
 
 # Start the Docker containers
 docker-compose up -d
