bcc0d239f5f3b7f1b87e37dcc8cfbe5cc4f9a514
renzora <106055871+renzora@users.noreply.github.com>
2024-03-09 00:54:08 +0000

    updated dockerfile and removed setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone https://github.com/renzora/dev
-cd dev
-git clone https://github.com/renzora/web
-git clone https://github.com/renzora/ws
-git clone https://github.com/renzora/server
-
-# Start the Docker containers
-docker-compose up -d
